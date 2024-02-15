3001b9a6274433e20e54f0f8facf3ff2fe0a3b73
jals413 <jalsthakor413@gmail.com>
2024-02-15 23:55:21 +0530

    CLI tests (cli-test-1708021521021)

--- app/server.go
+++ app/server.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 var mydb = make(map[string]string)
@@ -65,24 +64,25 @@ func handleCommands(s string, conn net.Conn) {
 		}
 		conn.Write([]byte("+" + lines[4] + "\r\n"))
 	case "SET":
-		if len(lines) != 7 || len(lines) != 11 {
+		if len(lines) != 7 {
 			conn.Write([]byte("-invalid command\r\n"))
 		}
 		if len(lines) == 7 {
 			mydb[lines[4]] = lines[6]
 			conn.Write([]byte("+OK\r\n"))
 		}
-		if len(lines) == 11 {
-			if(lines[8] == "PX") {
+		// if len(lines) == 11 {
+		// 	if(lines[8] == "PX") {
 
-				timer := time.After(time.Duration(lines[10]) * time.Millisecond)
+		// 		fmt.Println(lines[10])
+		// 		// timer := time.After(time.Duration(strconv.Atoi(lines[10])) * time.Millisecond)
 			
-				go func () {
-					<-timer
-					delete(mydb, lines[4])
-				}
-			}
-		}
+		// 		// go func () {
+		// 		// 	<-timer
+		// 		// 	delete(mydb, lines[4])
+		// 		// }
+		// 	}
+		// }
 
 	case "GET":
 		if len(lines) != 5 {
