76e6c0ffff504fbc1247157e12f0c8ea85f2c3c1
jals413 <jalsthakor413@gmail.com>
2024-02-15 23:46:47 +0530

    CLI tests (cli-test-1708021007458)

--- app/server.go
+++ app/server.go
@@ -75,7 +75,7 @@ func handleCommands(s string, conn net.Conn) {
 		if len(lines) == 11 {
 			if(lines[8] == "PX") {
 
-				timer := time.After(time.Duration(lines[10]) * time.Millisecond)
+				timer := time.After(time.Duration(Atoi(lines[10])) * time.Millisecond)
 			
 				go func () {
 					<-timer
